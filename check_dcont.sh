8003e5494f8a84c4dfb2568b7289d0b4bbe279d7
Tommi Muona <43796235+muonato@users.noreply.github.com>
2023-12-12 14:56:33 +0200

    Create check_dcont.sh

---
+++ check_dcont.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+#
+# muonato/check_dcont.sh @ GitHub (12-DEC-2023)
+#
+# Reports container statistics using docker stat,
+# compatible with Nagios monitoring as host plugin
+#
+# Usage:
+#       bash check_dcont.sh <container-name> [<container-name>] ...
+#
+#       Nagios nrpe configuration on host :
+#       command[check_dcont]=/path/to/plugins/check_dcont.sh $ARG1$
+#
+# Parameters:
+#       1..n: docker container name
+#
+# Examples:
+#       $ bash check_dcont.sh container-psql
+#       (check single docker container)
+#
+#       check_nrpe -H $HOSTADDRESS$ -c check_dcont -a 'container-psql container-redis'
+#       (nagios monitor line for two containers)
+#
+function container_stat () {
+    # Assign fn arguments
+    CONT=$1
+
+    # Get docker container memory stats
+    CMEM=$(sudo docker stats --format "{{.MemPerc}}" --no-stream $CONT 2>/dev/null)
+
+    # Strip percentage
+    CMEM=${CMEM%"%"}
+
+    # Get docker container CPU stats
+    CCPU=$(sudo docker stats --format "{{.CPUPerc}}" --no-stream $CONT 2>/dev/null)
+
+    # Strip percentage
+    CCPU=${CCPU%"%"}
+
+    if [[ -z "$CMEM" || -z "$CCPU" ]]; then
+        echo "CRITICAL - Docker container '$CONT' statistics failed"
+    elif [[ ${CMEM%.*} -ge 90 || ${CCPU%.*} -ge 90 ]]; then
+        echo "CRITICAL - Docker container '$CONT' CPU: $CCPU MEM: $CMEM"
+    elif [[ ${CMEM%.*} -ge 70 || ${CCPU%.*} -ge 70 ]]; then
+        echo "WARNING - Docker container '$CONT' CPU: $CCPU MEM: $CMEM"
+    elif [[ ${CMEM%.*} -ge 0 || ${CCPU%.*} -ge 0 ]]; then
+        echo "OK - Docker container '$CONT' CPU: $CCPU MEM: $CMEM"
+    else
